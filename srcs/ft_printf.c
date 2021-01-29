dbdddce75a2d5c30cd62809cb25179293cd3e540
LesChats <aimebaudot@gmail.com>
2021-01-29 03:44:00 +0100

    slowdown

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,37 +6,21 @@
 /*   By: abaudot <abaudot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/26 15:29:29 by abaudot           #+#    #+#             */
-/*   Updated: 2021/01/26 23:24:21 by abaudot          ###   ########.fr       */
+/*   Updated: 2021/01/29 03:42:06 by abaudot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static const char	*shr_pf(const char *s)
+static const char	*slow_shr(const char *s)
 {
-	size_t		word;
-	size_t		*sss;
-	int			i;
-
-	while ((size_t)s & 0b111)
-		if (*s++ == '%')
-			return (s - 1);
-		else if (*(s - 1) == '\0')
-			return (s - 1);
-	sss = (size_t *)s;
-	while (1)
+	while (*s)
 	{
-		word = *sss++;
-		if (((word - LOMAGIC) & (~word) & HIMAGIC) ||
-			(((word ^ PC) - LOMAGIC) & ~(word ^ PC) & HIMAGIC))
-		{
-			s = (char *)(sss - 1);
-			i = -1;
-			while (++i < 8)
-				if (s[i] == '%' || !s[i])
-					return (s + i);
-		}
+		if (*s == '%')
+			return (s);
+		++s;
 	}
+	return (s);
 }
 
 int					ft_printf(const char *format, ...)
@@ -46,7 +30,7 @@ int					ft_printf(const char *format, ...)
 
 	pf.format = format;
 	va_start(pf.ap, format);
-	while (*(convertion = shr_pf(pf.format)))
+	while (*(convertion = slow_shr(pf.format)))
 	{
 		buffer(pf.format, convertion - pf.format, 0);
 		pf.format = convertion + 1;
